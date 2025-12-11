345a4a27ca9060c6673b70e87ae8629f7f68f1f0
chouaib <choubbati@gmail.com>
2025-12-11 22:41:49 +0100

    creat nav

---
+++ include/nav.php
@@ -0,0 +1,29 @@
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container-fluid">
+
+    <a class="navbar-brand" href="#">ConnectSys</a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+
+      <ul class="navbar-nav ms-auto">
+
+        <li class="nav-item me-2">
+          <a href="login.php" class="btn btn-outline-light">Login</a>
+        </li>
+
+     
+        <li class="nav-item">
+          <a href="logout.php" class="btn btn-danger">register</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+
